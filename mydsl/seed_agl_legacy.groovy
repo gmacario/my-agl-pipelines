7900d189956e5392e099799a2cb538a7b743c803
Gianpaolo Macario <gmacario@gmail.com>
2016-02-15 16:40:12 +0100

    Update and rename seed_agl_MIRROR_fetchall_push.groovy to seed_agl_legacy.groovy

--- mydsl/seed_agl_legacy.groovy
+++ mydsl/seed_agl_legacy.groovy
@@ -1,6 +1,6 @@
 /* 
   Project: https://github.com/gmacario/jenkins-build-agl-distro
-  File:    mydsl/seed_agl_MIRROR_fetchall_push.groovy
+  File:    mydsl/sseed_agl_legacy.groovy
 */
   
 /*
@@ -8,12 +8,12 @@ Usage:
 
 Browse `${JENKINS_URL}`, then click **New Item**
 
-* Item Name: `seed_agl_MIRROR_fetchall_push`
+* Item Name: `seed_agl_legacy`
 * Type: Freestyle project
 
 then click **OK**.
 
-Browse `${JENKINS_URL}/job/seed_agl_MIRROR_fetchall_push/configure` and change
+Browse `${JENKINS_URL}/job/seed_agl_legacy/configure` and change
 
 * Build
   - Add build step > Process Job DSLs
@@ -22,14 +22,13 @@ Browse `${JENKINS_URL}/job/seed_agl_MIRROR_fetchall_push/configure` and change
   
 then click **Save**.
 
-Browse `${JENKINS_URL}/job/seed_agl_MIRROR_fetchall_push/`, then click **Build Now**
+Browse `${JENKINS_URL}/job/seed_agl_legacy/`, then click **Build Now**
 
-Result: Project `MIRROR-featchall-push` will be listed in `${JENKINS_URL}`
+Result: Folder `AGL-legacy` will be created in `${JENKINS_URL}`
+together with a few projects inside
 */
 
 def folderName = 'AGL-legacy'
-// def gitUrl = "https://github.com/gmacario/genivi-demo-platform"
-// def gitBranch = "qemux86-64"
 
 folder(folderName) {
   displayName('build.automotivelinux.org')
@@ -59,12 +58,13 @@ matrixJob(folderName + '/MIRROR-featchall-push') {
     label('yocto')
     text('MACHINE', 'qemux86', 'qemux86-64', 'porter')
   }
- 
+  steps {
+      shell "echo TODO"
+  }
 }
 
+/* OLD STUFF BELOW:
 
-
-/*
  label('yocto')
 
   scm {
